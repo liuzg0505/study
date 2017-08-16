59a04473910f05ca30d35b2efe354a6f837e86d5
liuzg0505 <2505847327@qq.com>
2017-08-16 20:50:56 +0800

    FlappyBird

---
+++ FlappyBird/land.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2017/8/14 0014.
+ */
+(function (window) {
+  function Land(options) {
+    this.x = options.x;
+    this.y = options.y;
+    this.img = options.img;
+    this.ctx = options.ctx;
+    this.width = this.img.width;
+    this.height = this.img.height;
+    this.speed = options.speed || 2;
+  }
+  Land.prototype = {
+    constructor: Land,
+    draw: function () {
+      //计算自己坐标
+      this.x -= this.speed;
+      
+      //判断是否完全在画布外面了
+      if(this.x <= -this.width) {
+        this.x += 4 * this.width;
+      }
+      
+      //绘制自己的图片
+      this.ctx.drawImage(this.img, this.x, this.y);
+      
+    }
+  }
+  
+  window.Land = Land;
+})(window);
